adad8d23b2d203da23aa86f5b834f1b4f0d141d6
Benoit Juin <benoit.juin@gmail.com>
2020-10-23 08:27:08 +0200

    fix cmake bullet debug vs release lib setting

--- cmake-extras/FindBullet.cmake
+++ cmake-extras/FindBullet.cmake
@@ -6,11 +6,32 @@ set(BULLET_ROOT_DIR $ENV{BULLET_ROOT_DIR})
 
 message("BULLET_ROOT_DIR :" ${BULLET_ROOT_DIR})
 
+if(NOT BULLET_ROOT_DIR)
+    message(FATAL_ERROR "The environement variable: BULLET_ROOT_DIR is not set")
+endif()
+
 set ( BULLET_FOUND 1 )
 set ( BULLET_USE_FILE       "${BULLET_ROOT_DIR}/lib/cmake/bullet/UseBullet.cmake" )
 set ( BULLET_DEFINITIONS    "" )
 set ( BULLET_INCLUDE_DIR    "${BULLET_ROOT_DIR}/include/bullet" )
 set ( BULLET_INCLUDE_DIRS   "${BULLET_ROOT_DIR}/include/bullet" )
-set ( BULLET_LIBRARIES      "LinearMath;Bullet3Common;BulletInverseDynamics;BulletCollision;BulletDynamics;BulletSoftBody" )
+set ( BULLET_LIBRARIES_REL      "LinearMath;Bullet3Common;BulletInverseDynamics;BulletCollision;BulletDynamics;BulletSoftBody" )
+
+foreach(name ${BULLET_LIBRARIES_REL})
+  set(BULLET_LIBRARIES_DBG ${BULLET_LIBRARIES_DBG} ${name}_Debug)
+endforeach(name)
+
+set(BULLET_LIBRARIES ${BULLET_LIBRARIES_DBG})
+
+if(CMAKE_BUILD_TYPE STREQUAL "Release")
+    set(BULLET_LIBRARIES ${BULLET_LIBRARIES_REL})
+endif()
+
+message("BULLET_LIBRARIES :" ${BULLET_LIBRARIES})
+message("BULLET_LIBRARIES_REL :" ${BULLET_LIBRARIES_REL})
+message("BULLET_LIBRARIES_DBG :" ${BULLET_LIBRARIES_DBG})
+
+
 set ( BULLET_LIBRARY_DIRS   "${BULLET_ROOT_DIR}/lib" )
 set ( BULLET_VERSION_STRING "3.05" )
+
